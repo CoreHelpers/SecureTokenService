8fcc5a0e7d34d9d9d78d4cc450e108c3f36c9455
Dirk Eisenberg <dirk.eisenberg@gmail.com>
2017-03-30 15:39:29 +0200

    adapted path

--- build.ps1
+++ build.ps1
@@ -9,4 +9,4 @@ Invoke-MSBuild
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
-exec { & dotnet pack .\src\YOUR_PROJECT_NAME -c Release -o .\artifacts --version-suffix=$revision }  
+exec { & dotnet pack .\CoreHelpers.AspNetCore.SecureTokenService\CoreHelpers.AspNetCore.SecureTokenService.csproj -c Release -o .\artifacts --version-suffix=$revision }  
