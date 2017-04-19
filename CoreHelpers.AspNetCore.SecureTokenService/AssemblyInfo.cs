9403d30a2a175e969df07c24d20f3f1845e89e79
Dirk Eisenberg <dirk.eisenberg@gmail.com>
2017-04-19 13:11:41 +0200

    added correct versioning

---
+++ CoreHelpers.AspNetCore.SecureTokenService/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: AssemblyCompany("Core Helpers")]
+[assembly: AssemblyProduct("SecureTokenService")]
+[assembly: AssemblyTitle("CoreHelpers Secure Token Service")]
+
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+
+#if (DEBUG)
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
